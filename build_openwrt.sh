b6f8f21a9a0226da95312fb29632468023936787
Oleksiy Mishchenko <amishchenko@gmail.com>
2025-09-07 20:23:12 +0200

    Build: Adapt workflow to mirror gwlim successful run

--- build_openwrt.sh
+++ build_openwrt.sh
@@ -1,19 +1,2 @@
-#!/bin/bash
-
-# Clone gwlim's pre-patched OpenWrt source for SFE
-git clone https://github.com/gwlim/openwrt.git -b 23.05-sfe openwrt
-cd openwrt
-
-# Patches are already included in the source. No manual patching needed.
-
-# Update feeds
-./scripts/feeds update -a
-./scripts/feeds install -a
-
-# Copy config
-cp ../wndr3700-v1-sfe-debug.config .config
-
-# Build firmware
-make defconfig
-make download -j$(nproc)
-make -j$(nproc)
+# This script is no longer needed, the build commands will be in the workflow file.
+# The gwlim repository is the build environment itself.

08a9dd2f4e6440a4670a7f6cd1dd138f88a64cd2
Oleksiy Mishchenko <amishchenko@gmail.com>
2025-09-07 18:01:55 +0200

    Revert "Refactor: Use custom feed for SFE instead of patches"
    This reverts commit fb3a83823542c293443eeffb18bb06a7eae1e996.

--- build_openwrt.sh
+++ build_openwrt.sh
@@ -4,7 +4,9 @@
 git clone https://github.com/openwrt/openwrt.git -b v23.05.3 openwrt
 cd openwrt
 
-# SFE patches are handled by a custom feed. No manual patching needed.
+# Apply SFE patches
+wget -O sfe.patch "https://git.codelinaro.org/clo/qsdk/oss/system/openwrt-patches-23.05/-/raw/NHSS.QSDK.12.4.5.r3/0052-netsupport.mk-fix-kmod-nss-sfe-configuration-issues.patch"
+git apply sfe.patch
 
 # Update feeds
 ./scripts/feeds update -a
